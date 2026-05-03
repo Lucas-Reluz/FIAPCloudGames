11aa20c92862761c137ac0e63556323762188c95
LucasReluz <lucas.reluz@localize.com.br>
2026-05-03 03:58:54 -0300

    Adicionar arquivos de projeto.

---
+++ FIAPCloudGames.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using FIAPCloudGames.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAPCloudGames.Domain.Entities
+{
+    public class User
+    {
+        public User(string name, Email email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Name { get; set; }
+        public Email Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
